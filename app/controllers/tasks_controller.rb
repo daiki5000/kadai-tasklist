79191fa665e791dcdde487db52309fddd6dffdde
daiki.takeishi <tw.dia3@gmail.com>
2021-02-08 09:41:45 +0000

    tasklist login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action:require_user_logged_in
+  before_action :correct_user, only: [:destroy,:update]
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
+    @task = Task.find_by(id: params[:id])
   end
   
   def new
@@ -12,13 +14,14 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクを正常に作成出来ました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクが作成出来ませんでした'
-      render :new
+      render 'toppage/index'
     end
   end
   
@@ -38,16 +41,22 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
-    flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'タスクを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
   def task_params
-    params.require(:task).permit(:content,:status)
+    params.require(:task).permit(:content,:status,:user)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
